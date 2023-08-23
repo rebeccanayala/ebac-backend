c88cd86d7e6d134f296f03051e8e80adaca87da1
Rebecca Nayala <140104442+rebeccanayala@users.noreply.github.com>
2023-08-23 20:56:01 -0300

    Create Aluno.java

---
+++ mod7/Aluno.java
@@ -0,0 +1,12 @@
+public class Aluno { //Criando uma classe 
+    
+    int id;
+    String nome;  //Criando variáveis
+    
+
+    public static void main(String[] args) { //Criando o método principal
+        Aluno a1 = new Aluno(); //Criando um objeto
+        System.out.print(a1.nome); //Imprimindo resultado
+}
+
+

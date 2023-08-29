67dc24588fadfcfabd4c63b8e71c09102faafc3c
Rebecca Nayala <140104442+rebeccanayala@users.noreply.github.com>
2023-08-29 10:41:16 -0300

    Create mod9

---
+++ mod9/tarefa-wrapper/wrapper.java
@@ -0,0 +1,13 @@
+public class wrapper {
+
+    public static void main(String[] args) {
+        convertWrapper();
+    }
+
+    private static void convertWrapper() {
+        Integer num1 = 10;
+
+        System.out.println(num1);
+    }
+
+}

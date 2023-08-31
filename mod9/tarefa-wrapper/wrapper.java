3ee8dfb1d0dbd7768dbc94361cf59bbdc9d13aed
Rebecca Nayala <140104442+rebeccanayala@users.noreply.github.com>
2023-08-31 13:37:29 -0300

    Update wrapper.java

--- mod9/tarefa-wrapper/wrapper.java
+++ mod9/tarefa-wrapper/wrapper.java
@@ -1,13 +1,17 @@
+import java.util.Scanner;
+
 public class wrapper {
-
+    
     public static void main(String[] args) {
-        convertWrapper();
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.print("Digite um valor inteiro:");
+        int intNumber = scanner.nextInt();
+
+        Integer integerWrapper = Integer.valueOf(intNumber);
+
+        System.out.println(integerWrapper + " foi o valor que você digitou." );
+
+        scanner.close();
     }
-
-    private static void convertWrapper() {
-        Integer num1 = 10;
-
-        System.out.println(num1);
-    }
-
 }

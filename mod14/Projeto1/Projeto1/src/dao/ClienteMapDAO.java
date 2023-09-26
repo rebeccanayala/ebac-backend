e2f22b52510d6a025ad7b51110803e9fa3ee4ac0
Rebecca Nayala <140104442+rebeccanayala@users.noreply.github.com>
2023-09-26 07:38:18 -0300

    Add mod14

---
+++ mod14/Projeto1/Projeto1/src/dao/ClienteMapDAO.java
@@ -0,0 +1,48 @@
+package dao;
+
+import domain.Cliente;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ClienteMapDAO implements IClienteDAO{
+
+    private Map<Long, Cliente> map;
+    public ClienteMapDAO() {
+        this.map = new HashMap<>();
+    }
+
+    @Override
+    public Boolean cadastrar(Cliente cliente) {
+        if (this.map.containsKey(cliente.getCpf())) {
+            return false;
+        }
+        this.map.put(cliente.getCpf(), cliente);
+        return null;
+    }
+
+    @Override
+    public void excluir(Long cpf) {
+        Cliente clienteCadastrado = this.map.get(cpf);
+
+        if (clienteCadastrado != null) {
+            this.map.remove(clienteCadastrado.getCpf(), clienteCadastrado);
+        }
+    }
+
+    @Override
+    public void alterar(Cliente cliente) {
+
+    }
+
+    @Override
+    public Cliente consultar(Long cpf) {
+        return null;
+    }
+
+    @Override
+    public Collection<Cliente> buscarTodos() {
+        return null;
+    }
+}

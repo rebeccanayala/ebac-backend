1208b3506b551b1101f5d182da5d59d5f42b2a53
Rebecca Nayala <140104442+rebeccanayala@users.noreply.github.com>
2023-11-09 16:00:35 -0300

    Update mod23

--- mod23/Pessoa/test/PrimeiroTeste.java
+++ mod23/Pessoa/test/PrimeiroTeste.java
@@ -8,6 +8,18 @@ public class PrimeiroTeste {
 
     @Test
     public void mulheres() {
+        List<Pessoa> pessoas = new ArrayList<>();
+        pessoas.add(new Pessoa("Armando", "M"));
+        pessoas.add(new Pessoa("Bárbara", "F"));
+        pessoas.add(new Pessoa("Carlos", "M"));
+        pessoas.add(new Pessoa("Daniela", "F"));
+        pessoas.add(new Pessoa("Emanuel", "M"));
+        pessoas.add(new Pessoa("Fabiana", "F"));
+        pessoas.add(new Pessoa("Gabriel", "M"));
+        pessoas.add(new Pessoa("Helen", "F"));
+        pessoas.add(new Pessoa("Irene", "F"));
+        pessoas.add(new Pessoa("João", "M"));
+
         List<Pessoa> mulheres = pessoas.stream()
                 .filter(p -> p.getSexo().equals("F"))
                 .collect(Collectors.toList());

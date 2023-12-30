e1d7c1b8fc03373ca0aa01b1eab19e683139cba9
Rebecca Nayala <140104442+rebeccanayala@users.noreply.github.com>
2023-12-30 01:14:12 -0300

    Adding mod29

---
+++ mod29/Cliente/src/dao/IClienteDAO.java
@@ -0,0 +1,12 @@
+package dao;
+
+import domain.Cliente;
+
+
+public interface IClienteDAO {
+    public Integer cadastrar(Cliente cliente) throws Exception;
+
+    public Cliente consultar(String codigo) throws Exception;
+
+    public Integer excluir(Cliente clienteBD) throws Exception;
+}

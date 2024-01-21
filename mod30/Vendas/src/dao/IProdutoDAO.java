693d2af4730c25f0316f0ecefda719fd133b0870
Rebecca Nayala <140104442+rebeccanayala@users.noreply.github.com>
2024-01-21 16:12:43 -0300

    Deleting mod30 and mod31

--- mod30/Vendas/src/dao/IProdutoDAO.java
+++
@@ -1,7 +0,0 @@
-package dao;
-
-import dao.IGenericDAO;
-import domain.Produto;
-
-public interface IProdutoDAO extends IGenericDAO<Produto, String>{
-}
--- mod30/Vendas/src/dao/Persistente.java
+++
@@ -1,7 +0,0 @@
-package dao;
-
-public interface Persistente {
-    public Long getId();
-
-    public void setId(Long id);
-}

196fff9f30675ecbbcf7a256a63c0fd23d440c74
Rebecca Nayala <140104442+rebeccanayala@users.noreply.github.com>
2024-01-25 18:30:06 -0300

    Adding Recursividade.java

---
+++ others/mod2/Recursividade/src/exercicio1/Recursividade.java
@@ -0,0 +1,18 @@
+package exercicio1;
+
+public class Recursividade {
+    public static void main(String[] args) {
+      int numero = 4;
+      long resultado = calcularFatorial(numero);
+
+        System.out.println(resultado);
+    }
+
+    private static long calcularFatorial(int n) {
+        if (n == 0  || n == 1) {
+            return 1;
+        } else {
+            return n * calcularFatorial(n -1);
+        }
+    }
+}
---
+++ others/mod2/Recursividade/src/exercicio2/Dinamico.java
@@ -0,0 +1,6 @@
+package exercicio2;
+
+import java.util.HashMap;
+
+public class Dinamico {
+}

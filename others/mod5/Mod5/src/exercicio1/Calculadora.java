c64ab8c81b0d099a2c87b4cd0e1ac95588d3e200
Rebecca Nayala <140104442+rebeccanayala@users.noreply.github.com>
2024-01-28 22:29:08 -0300

    Adding documentation

--- others/mod5/Mod5/src/exercicio1/Calculadora.java
+++ others/mod5/Mod5/src/exercicio1/Calculadora.java
@@ -1,19 +1,25 @@
 package exercicio1;
 
+/* Classe que representa uma calculadora simples. */
 public class Calculadora {
 
+
+    /* Realiza a adição de dois números inteiros.*/
     public int adicionar(int a, int b) {
         return a + b;
     }
 
+    /* Realiza a subtração de dois números inteiros.*/
     public int subtrair(int a, int b) {
         return a - b;
     }
 
+    /* Realiza a multiplicação de dois números inteiros.*/
     public int multiplicar (int a, int b) {
         return a * b;
     }
 
+    /* Realiza a divisão de dois números inteiros.*/
     public int dividir (int a, int b) {
         if (b == 0) {
             throw new ArithmeticException("Não se pode dividir por zero.");
